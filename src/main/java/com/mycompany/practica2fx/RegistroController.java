ec28d6c4a67f3898a95b95466d43ab74b45c4360
Pablofu9 <pablofuertesruiz@gmail.com>
2022-10-25 10:19:29 +0200

    Vista uptdate

--- src/main/java/com/mycompany/practica2fx/RegistroController.java
+++ src/main/java/com/mycompany/practica2fx/RegistroController.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import modelo.CRUD_Usuarios;
@@ -23,29 +24,38 @@ import modelo.Usuarios;
 public class RegistroController implements Initializable {
 
     Connection con;
-    
+
     @FXML
     private TextField registroDni, registroNombre, registroApellido, registroPass;
-    
+
     @FXML
     private Button btnRegistro, btnVolver;
-    
+
     @FXML //Boton para volver a la pantalla de login
-    private void volver() throws IOException{
+    private void volver() throws IOException {
         App.setRoot("Login");
     }
-    
+
+    //Boton para registrarte
     @FXML
-    private void registrarUser(){
-       
-        Usuarios user1 = new Usuarios(registroNombre.getText(), registroApellido.getText(), registroDni.getText(), registroPass.getText());
-        CRUD_Usuarios.insertarUsuario(con, user1);
+    private void registrarUser() {
+        //Esto nos va a sacar una alerta de un error si dejas algun campo vacio
+        if (registroNombre.getText().isEmpty() || registroApellido.getText().isEmpty() || registroDni.getText().isEmpty() || registroPass.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Error");
+            alert.setContentText("Hay algun campo vacio");
+            alert.showAndWait();
+        } else {
+            Usuarios user1 = new Usuarios(registroNombre.getText(), registroApellido.getText(), registroDni.getText(), registroPass.getText());
+            CRUD_Usuarios.insertarUsuario(con, user1);
+        }
+
     }
-    
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
