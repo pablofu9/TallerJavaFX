b90834d0fdcfb042f083a96a10fde7eeae7baf97
Pablofu9 <pablofuertesruiz@gmail.com>
2022-10-25 09:42:33 +0200

    Vista uptdate

---
+++ src/main/java/com/mycompany/practica2fx/LoginController.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.practica2fx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author furu9
+ */
+public class LoginController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    @FXML
+    private Button botonIniciar, botonRegistro;
+    
+    @FXML
+    private TextField txtDni;
+    
+    @FXML
+    private PasswordField txtPass;
+    
+    @FXML
+    private Label lblDNI, lblPass;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.mycompany.practica2fx {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.base;
     
     opens com.mycompany.practica2fx to javafx.fxml;
     exports com.mycompany.practica2fx;
